ebed7c4f1ad7d4363728f09564d69a0acee0bb10
CarlosApestigue <can0005@alu.medac.es>
2026-02-06 17:30:07 +0100

    feat: actualizar mensaje de la función saludar a versión 2

--- proyecto-daw/js/app.js
+++ proyecto-daw/js/app.js
@@ -1,3 +1,5 @@
 function saludar() {
+console.log("Función ejecutada correctamente - versión 2");
+    alert("Sistema DevWebPro funcionando correctamente v2");
     alert("Proyecto funcionando correctamente. ¡Hola DevWebPro!");
 }
